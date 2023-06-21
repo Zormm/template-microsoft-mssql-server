4d25d3588acf7664e443ae4fa97015828e1efc60
Jona Krampe <Krampe.Jona@gmail.com>
2023-06-21 17:06:22 +0000

    Today's Progress

--- tutorial.sql
+++ tutorial.sql
@@ -14,7 +14,7 @@ CREATE Table Produkte(
 );
 
 CREATE Table Kaufhistorie (
-    Dateum date,
+    Datum date,
     Anzahl int
 );
 
@@ -39,7 +39,7 @@ ALTER TABLE Produkte
 ALTER COLUMN Preis DECIMAL(10,2)
 
 /*markdown
-# Die Ursprüngliche Präzision haben wir verloren. Diese stellen wir nun wieder her mit dem Update Befehl und ändern den bestehenden Preis der Milch zuurück auf den Präzisen Wert von Preis = 1,99 €
+# Die Ursprüngliche Präzision haben wir verloren. Diese stellen wir nun wieder her mit dem Update Befehl und ändern den bestehenden Preis der Milch zurück auf den Präzisen Wert von Preis = 1,99 €
 */
 
 UPDATE Produkte
@@ -52,9 +52,66 @@ Die Produkte Tabelle erhält einen Primärschlüssel und die Kaufhistorie einen 
 
 */
 
+ALTER Table Produkte
+ADD ID int IDENTITY(1,1)
+
+
+ALTER Table Kaufhistorie
+ADD PRIMARY KEY (ID);
+
 /*markdown
 # Prüfen, obs erfolgreich war
 */
+
+SELECT *
+From Produkte
+
+/*markdown
+# Neu Anlegen der Tabelle Produkte mit den Spalten in der richtigen Reihenfolge
+*/
+
+begin transaction
+
+SELECT *
+INTO #tempTable
+From Produkte;
+
+drop table Produkte;
+
+Create Table Produkte(
+    ID int IDENTITY(1,1),
+    Name nvarchar(max),
+    "Preis" DECIMAL(10,2),
+    PRIMARY KEY (ID)
+)
+
+SET IDENTITY_INSERT Produkte ON;
+
+INSERT INTO Produkte(ID, Name, preis)
+Select ID, Name, Preis
+From #tempTable;
+
+SET IDENTITY_INSERT Produkte OFF;
+
+Select * 
+From Produkte;
+
+commit transaction;
+
+/*markdown
+# Prüfen, ob die TempTable auch weg ist
+*/
+
+Select * 
+From #tempTable
+
+/*markdown
+# Prüfen, ob die Produkte Tabelle nun korrekt aussieht
+*/
+
+
+Select * 
+From Produkte;
 
 /*markdown
 # Anlegen geeigneter Datensätze
