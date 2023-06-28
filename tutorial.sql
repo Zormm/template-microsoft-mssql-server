4c6ce6eb42b4f85d7145996c9f1dd49501e171e4
Jona Krampe <Krampe.Jona@gmail.com>
2023-06-28 17:08:21 +0000

    Added new Directoires + More Dataset

--- tutorial.sql
+++ tutorial.sql
@@ -29,7 +29,9 @@ SELECT * FROM Produkte
 */
 
 INSERT INTO Produkte (Name, Preis)
-VALUES ('Milch', 1.99)
+VALUES ('Schokolade', 1.29),
+('Spaghetti', 0.69), 
+('Milch', 1.99)
 
 /*markdown
 # Korrektur der Produkte Tabelle
@@ -49,7 +51,6 @@ WHERE Name = 'Milch'
 /*markdown
 # Verändern der Produkte und Kaufhistorie Tabelle
 Die Produkte Tabelle erhält einen Primärschlüssel und die Kaufhistorie einen Fremdschlüssel zur Produkte Tabelle
-
 */
 
 ALTER Table Produkte
@@ -71,31 +72,23 @@ From Produkte
 */
 
 begin transaction
-
 SELECT *
 INTO #tempTable
 From Produkte;
-
 drop table Produkte;
-
 Create Table Produkte(
     ID int IDENTITY(1,1),
     Name nvarchar(max),
     "Preis" DECIMAL(10,2),
     PRIMARY KEY (ID)
 )
-
 SET IDENTITY_INSERT Produkte ON;
-
 INSERT INTO Produkte(ID, Name, preis)
 Select ID, Name, Preis
 From #tempTable;
-
 SET IDENTITY_INSERT Produkte OFF;
-
 Select * 
 From Produkte;
-
 commit transaction;
 
 /*markdown
@@ -115,10 +108,35 @@ From Produkte;
 
 /*markdown
 # Anlegen geeigneter Datensätze
-
 */
+
+ALTER TABLE Kaufhistorie
+add ProduktId int
+
+INSERT INTO Kaufhistorie(Datum, Anzahl, ProduktID)
+values ('1.2.23', 5, 2),
+('1.2.23', 1, 1), ('1.2.23', 1, 4)
+
+Select *
+From Kaufhistorie
+
+SELECT *
+FROM Kaufhistorie k
+full OUTER JOIN Produkte p
+ON k.ProduktId = p.ID;
+
+Select *
+from Kaufhistorie, Produkte
+where produktid = id
 
 /*markdown
 # Prüfen, obs erfolgreich war
+*/
 
+/*markdown
+# Spalte 'produktid' aus Tabelle Kaufhistorie umwandeln in Foreign Key
+*/
+
+/*markdown
+# Normalisierung bis 3. Normalform anschauen
 */
